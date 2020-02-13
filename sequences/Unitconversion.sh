525d505a4cdb9141a3a4f090b27a27b76e578586
Nileshlad <ladnilesh1994@gmail.com>
2020-02-13 12:14:47 +0530

    MODIFIED:To convert area into acres

--- sequences/Unitconversion.sh
+++ sequences/Unitconversion.sh
@@ -13,3 +13,7 @@ echo  $feet "feet is equal to " $secondResult "inch"
 #TO FEET TO METER CONVERSION
 meter=`echo  "scale=2; (60/3.2808) * (40/3.2808)" | bc`
 echo "60feet * 40feet is equal to "$meter "meter"
+
+#TO CONVERT AREA INTO ACRES
+area=`echo "scale=2; $meter/4047 *25" | bc`
+echo "Area of 25 plts in acres are "$area
