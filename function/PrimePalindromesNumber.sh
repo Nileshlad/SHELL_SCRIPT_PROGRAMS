290e034adaf37a7c9352002f749e5461e7aa83a5
Nileshlad <ladnilesh1994@gmail.com>
2020-02-13 17:20:00 +0530

    MODIFIED:To check number prime or not prime and palindrom number

--- function/PrimePalindromesNumber.sh
+++ function/PrimePalindromesNumber.sh
@@ -23,9 +23,32 @@ function isPrime()
 	then
 		echo $number is Prime Number
 
-		#TO FUNCTION PRINT TO CHECK PALINDROME OF PRIME NUMBER
+		#TO PRINT FUNCTION 
 		echo "$( isPalindrome $number )"
 	else
 		echo $number is not prime Number
 	fi
 }
+
+#TO FUNCTION PALINDROME
+function isPalindrome()
+{
+	while [ $number -ne 0 ]
+	do
+		remainder=$(( $number % 10 ))
+		reverse=$(( reverse * 10 +remainder ))
+		number=$(( $number / 10 ))
+	done
+	echo $reverse
+
+	if [ $originalNumber -eq $reverse ]
+	then
+		echo "Number is Palindrome"
+	else
+		echo "Number is not Palindrome"
+	fi
+}
+
+# TO PRINT FUNCTION 
+echo "$( isPrime $number )"
+echo "$( isPrime $reverse )"
