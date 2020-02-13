35c78e53908804f190fdeb8ee6ddad774e978671
Nileshlad <ladnilesh1994@gmail.com>
2020-02-13 14:32:56 +0530

    ADDED:To Check the single Digit number

---
+++ ifelseifstatement/SingleDigitNumber.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x 
+#TO PRINT THE SINGLE 0 TO 9 NUMBER
+echo " Enter the single digit number "
+read number
+
+if [ $number == 1 ];
+then
+     echo "one"
+elif [ $number == 2 ]
+then
+     echo "two"
+elif [ $number == 3 ]
+then
+     echo "three"
+elif [ $number == 4 ]
+then
+     echo "four"
+elif [ $number == 5 ]
+then
+     echo "five"
+elif [ $number == 6 ]
+then
+     echo "six"
+elif [ $number == 7 ]
+then
+     echo "seven"
+elif [ $number == 8 ]
+then
+     echo "eight"
+elif [ $number == 9 ]
+then
+     echo "nine"
+fi
