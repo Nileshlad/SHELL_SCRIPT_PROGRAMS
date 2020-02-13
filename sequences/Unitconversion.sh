5e3ce039195db313303cfa8f81c76d62cea3e750
Nileshlad <ladnilesh1994@gmail.com>
2020-02-13 12:10:56 +0530

    MODIFIED:To Meter to inch

--- sequences/Unitconversion.sh
+++ sequences/Unitconversion.sh
@@ -5,7 +5,11 @@ read -p "Enter the Inch :" inch
 firstResult=$(( $inch/12 ))
 echo $inch " Inch is equal to" $firstResult "Feet"
 
-#To CONVERT FEET TO INCH
+#TO CONVERT FEET TO INCH
 read -p "Enter the feet :" feet
 secondResult=$(( $feet*12 ))
 echo  $feet "feet is equal to " $secondResult "inch"
+
+#TO FEET TO METER CONVERSION
+meter=`echo  "scale=2; (60/3.2808) * (40/3.2808)" | bc`
+echo "60feet * 40feet is equal to "$meter "meter"
