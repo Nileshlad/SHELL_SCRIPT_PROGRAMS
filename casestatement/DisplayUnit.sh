ef50a71e8b7099fedd01d73f37e4df4b6e26a544
Nileshlad <ladnilesh1994@gmail.com>
2020-02-13 15:22:00 +0530

    ADDED:To print units

---
+++ casestatement/DisplayUnit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+# READ USER INPUT
+read -p "Enter the Number: " number
+
+# DISPLAY DIGIT PLACE
+case $number in
+	1)
+		echo Unit
+	;;
+	10)
+		echo Ten
+	;;
+	100)
+		echo Hundread
+	;;
+	1000)
+		echo Thousand
+	;;
+	*)
+		echo Enter Numbers 1, 10, 100 or 1000 only.
+	;;
+esac
