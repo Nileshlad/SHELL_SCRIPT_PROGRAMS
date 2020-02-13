d57de485bbeaef8cf29f395f13ce277dc1a5f11a
Nileshlad <ladnilesh1994@gmail.com>
2020-02-13 12:07:28 +0530

    MODIFIED:To feet to inch

--- sequences/Unitconversion.sh
+++ sequences/Unitconversion.sh
@@ -4,3 +4,8 @@
 read -p "Enter the Inch :" inch
 firstResult=$(( $inch/12 ))
 echo $inch " Inch is equal to" $firstResult "Feet"
+
+#To CONVERT FEET TO INCH
+read -p "Enter the feet :" feet
+secondResult=$(( $feet*12 ))
+echo  $feet "feet is equal to " $secondResult "inch"
