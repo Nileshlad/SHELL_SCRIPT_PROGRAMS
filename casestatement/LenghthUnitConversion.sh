244415c121921d6c5dce81674018b5f02b8e8e95
Nileshlad <ladnilesh1994@gmail.com>
2020-02-14 11:34:23 +0530

    MODIFIED:To changes cases

--- casestatement/LenghthUnitConversion.sh
+++ casestatement/LenghthUnitConversion.sh
@@ -18,23 +18,23 @@ case $choice in
 		#TO  FEET TO INCH
 		result=$(echo "scale=2; $length * 12" | bc)
 		echo $length Feet = $result Inch
-	;;
+		;;
 	2)
 		#TO FEET TO METER
 		result=$(echo "$length * 0.3048" | bc)
 		echo $length Feet = $result Meter
-	;;
+		;;
 	3)
 		#TO INCH TO FEET
 		result=`expr "$length * 0.083" | bc`
 		echo $length Inch = $result Feet
-	;;
+		;;
 	4)
 		#TO METER TO FEET
 		result=`expr "$length * 3.28" | bc`
 		echo $length Meter = $result Feet
-	;;
+		;;
 	*)
 		echo Enter Valid Number
-	;;
+		;;
 esac
