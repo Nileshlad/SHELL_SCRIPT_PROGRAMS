d44879b4c94b8baff2ef9af4d1b367f80a1d6081
Nileshlad <ladnilesh1994@gmail.com>
2020-02-14 11:32:31 +0530

    MODIFIED:To changes cases

--- casestatement/DisplayUnit.sh
+++ casestatement/DisplayUnit.sh
@@ -7,17 +7,17 @@ read -p "Enter the Number: " number
 case $number in
 	1)
 		echo Unit
-	;;
+		;;
 	10)
 		echo Ten
-	;;
+		;;
 	100)
 		echo Hundread
-	;;
+		;;
 	1000)
 		echo Thousand
-	;;
+		;;
 	*)
 		echo Enter Numbers 1, 10, 100 or 1000 only.
-	;;
+		;;
 esac
